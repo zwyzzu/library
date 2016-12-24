7b50ddbd934a9e4b616ae51d66d57b0f014147c8
zhangwy <zhangweiya@guazi.com>
2016-12-24 18:22:05 +0800

    ClearEditText

---
+++ widget/src/com/zwy/widget/ClearEditText.java
@@ -0,0 +1,129 @@
+package com.zwy.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.EditText;
+
+/**
+ * Created by 张维亚(zhangwy) on 2016/12/24 下午4:54.
+ * Updated by zhangwy on 2016/12/24 下午4:54.
+ * Description 带有清理按钮的edit text
+ */
+public class ClearEditText extends EditText implements View.OnFocusChangeListener, TextWatcher {
+    /**
+     * 删除按钮的引用
+     */
+    private Drawable mClearDrawable;
+    /**
+     * 控件是否有焦点
+     */
+    private boolean hasFocus;
+
+    public ClearEditText(Context context) {
+        super(context);
+        this.init();
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.init();
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init();
+    }
+
+    private void init() {
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            mClearDrawable = getResources().getDrawable(R.drawable.icon_clear);
+        }
+        if (mClearDrawable != null){
+            mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+        }
+        setClearIconVisible(false);
+        setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (getCompoundDrawables()[2] != null) {
+                if (event.getX() > (getWidth() - getTotalPaddingRight()) && event.getX() < getWidth()) {
+                    this.setText("");
+                }
+            }
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 当ClearEditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
+     */
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        this.hasFocus = hasFocus;
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+    }
+
+    /**
+     * 设置清除图标的显示与隐藏，调用setCompoundDrawables为EditText绘制上去
+     */
+    protected void setClearIconVisible(boolean visible) {
+        Drawable right = visible ? mClearDrawable : null;
+        setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
+    }
+
+    /**
+     * 当输入框里面内容发生变化的时候回调的方法
+     */
+    @Override
+    public void onTextChanged(CharSequence s, int start, int count, int after) {
+        if (hasFocus) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+    /**
+     * 设置晃动动画
+     */
+    public void startShake() {
+        this.startAnimation(shakeAnimation(5));
+    }
+
+    /**
+     * 晃动动画
+     *
+     * @param counts 1秒钟晃动多少下
+     */
+    public static Animation shakeAnimation(int counts) {
+        Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+        translateAnimation.setInterpolator(new CycleInterpolator(counts));
+        translateAnimation.setDuration(1000);
+        return translateAnimation;
+    }
+
+}
