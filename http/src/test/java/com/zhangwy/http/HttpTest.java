438c03afd80f3eaf56a14371447473d9355cac95
zhangwy <1010482327@qq.com>
2017-03-31 14:57:38 +0800

    Update test

--- http/src/test/java/com/zhangwy/http/HttpTest.java
+++ http/src/test/java/com/zhangwy/http/HttpTest.java
@@ -1,5 +1,7 @@
 package com.zhangwy.http;
 
+import com.zhangwy.utils.Logger;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,30 +25,59 @@ public class HttpTest {
 
     @Test
     public void defaultHttpClient() throws Exception {
-
-    }
-
-    @Test
-    public void newHttpClient() throws Exception {
-        Http.newHttpClient().get("", new HttpHandler() {
+        Http.defaultHttpClient().get("https://www.baidu.com/", new HttpHandler() {
             @Override
             public void onSuccess(HttpRequest req, HttpResponse resp) {
-
+                Logger.d("success" + new String(resp.getContent()));
+                System.out.println("success" + new String(resp.getContent()));
             }
 
             @Override
             public void onFailed(HttpRequest req, HttpResponse resp) {
-
+                Logger.d("failed" + resp.getMsg());
+                System.out.println("failed" + resp.getMsg());
             }
 
             @Override
             public void onError(HttpRequest req, String errMsg) {
-
+                Logger.d("error" + errMsg);
+                System.out.println("error" + errMsg);
             }
 
             @Override
             public void onRetry(HttpRequest req, String reason) {
+                Logger.d("retry");
+                System.out.println("retry");
+            }
+        });
+        System.out.println("default");
+    }
 
+    @Test
+    public void newHttpClient() throws Exception {
+        Http.newHttpClient().get("https://www.baidu.com/", new HttpHandler() {
+            @Override
+            public void onSuccess(HttpRequest req, HttpResponse resp) {
+                Logger.d("success" + new String(resp.getContent()));
+                System.out.println("success" + new String(resp.getContent()));
+            }
+
+            @Override
+            public void onFailed(HttpRequest req, HttpResponse resp) {
+                Logger.d("failed" + resp.getMsg());
+                System.out.println("failed" + resp.getMsg());
+            }
+
+            @Override
+            public void onError(HttpRequest req, String errMsg) {
+                Logger.d("error" + errMsg);
+                System.out.println("error" + errMsg);
+            }
+
+            @Override
+            public void onRetry(HttpRequest req, String reason) {
+                Logger.d("retry");
+                System.out.println("retry");
             }
         });
     }
