4b0761097b1751e58735aaaa824f76fb769acc5d
zhangwy <1010482327@qq.com>
2017-04-06 14:36:27 +0800

    update Logger
    修改根据级别打印日志

--- utils/src/com/zhangwy/utils/Logger.java
+++ utils/src/com/zhangwy/utils/Logger.java
@@ -2,16 +2,7 @@
 * Module Name:com.zwy.demo
 * File Name:Log.java
 * Author: 张维亚
-* Copyright 2007-, Funshion Online Technologies Ltd.
-* All Rights Reserved
-* 版权 2007-，北京风行在线技术有限公司
-* 所有版权保护
-* This is UNPUBLISHED PROPRIETARY SOURCE CODE of Funshion Online Technologies Ltd.;
-* the contents of this file may not be disclosed to third parties, copied or
-* duplicated in any form, in whole or in part, without the prior written
-* permission of Funshion Online Technologies Ltd.
-* 这是北京风行在线技术有限公司未公开的私有源代码。本文件及相关内容未经风行在线技术有
-* 限公司事先书面同意，不允许向任何第三方透露，泄密部分或全部; 也不允许任何形式的私自备份。
+* Copyright 2014-.
 ***************************************************************************************/
 package com.zhangwy.utils;
 
@@ -26,62 +17,161 @@ import android.util.Log;
  **/
 public class Logger {
 
-	public static boolean PRINT_LOG = true;
-	public static boolean PRINT_LOG_2FILE = true;
-	public static String Tag = "logger";
+	private static final String Tag = "logger";
+	private static final boolean PRINT_LOG = true;
+	private static final boolean PRINT_LOG_2FILE = true;
 
-	public static void i(String tag, String msg) {
-		if (printlog(msg)) {
-			Log.i(tag, msg);
-			printLog("i", tag, msg);
+	public static final int LEVEL_ASSERT = Log.ASSERT;//7不打印
+	public static final int LEVEL_ERROR = Log.ERROR;//6
+	public static final int LEVEL_WARN = Log.WARN;//5
+	public static final int LEVEL_INFO = Log.INFO;//4
+	public static final int LEVEL_DEBUG = Log.DEBUG;//3
+	public static final int LEVEL_VERBOSE = Log.VERBOSE;//2
+
+	private static int level = PRINT_LOG ? LEVEL_VERBOSE : LEVEL_ASSERT;
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public static void e(String msg) {
+		e(Tag, msg);
+	}
+
+	public static void e(String msg, Throwable e) {
+		e(Tag, msg, e);
+	}
+
+	public static void e(String tag, String msg) {
+		if (printLog(LEVEL_ERROR, msg)) {
+			Log.e(tag, msg);
+			loggerWriter("e", tag, msg);
+		}
+	}
+
+	public static void e(String tag, String msg, Throwable e) {
+		final String eMsg = throwableMsg(msg, e);
+		if (printLog(LEVEL_ERROR, eMsg)) {
+			Log.e(tag, msg, e);
+			loggerWriter("e", tag, eMsg);
+		}
+	}
+
+	public static void w(String msg){
+		w(Tag, msg);
+	}
+
+	public static void w(String msg, Throwable e) {
+		w(Tag, msg, e);
+	}
+
+	public static void w(String tag, String msg) {
+		if (printLog(LEVEL_WARN, msg)) {
+			Log.w(tag, msg);
+			loggerWriter("w", tag, msg);
+		}
+	}
+
+	public static void w(String tag, String msg, Throwable e) {
+		final String eMsg = throwableMsg(msg, e);
+		if (printLog(LEVEL_WARN, eMsg)) {
+			Log.w(tag, msg, e);
+			loggerWriter("w", tag, eMsg);
 		}
 	}
 
 	public static void i(String msg) {
-		if (printlog(msg)) {
-			Log.i(Tag, msg);
-			printLog("i", Tag, msg);
+		i(Tag, msg);
+	}
+
+	public static void i(String msg, Throwable e) {
+		i(Tag, msg, e);
+	}
+
+	public static void i(String tag, String msg) {
+		if (printLog(LEVEL_INFO, msg)) {
+			Log.i(tag, msg);
+			loggerWriter("i", tag, msg);
 		}
 	}
 
-	public static void d(String tag, String msg) {
-		if (printlog(msg)) {
-			Log.d(tag, msg);
-			printLog("d", tag, msg);
+	public static void i(String tag, String msg, Throwable e) {
+		final String eMsg = throwableMsg(msg, e);
+		if (printLog(LEVEL_INFO, eMsg)) {
+			Log.i(tag, msg, e);
+			loggerWriter("i", tag, eMsg);
 		}
 	}
 
 	public static void d(String msg) {
-		if (printlog(msg)) {
-			Log.d(Tag, msg);
-			printLog("d", Tag, msg);
+		d(Tag, msg);
+	}
+
+	public static void d(String msg, Throwable e) {
+		d(Tag, msg, e);
+	}
+
+	public static void d(String tag, String msg) {
+		if (printLog(LEVEL_DEBUG, msg)) {
+			Log.d(tag, msg);
+			loggerWriter("d", tag, msg);
 		}
 	}
 
-	public static void e(String tag, String msg) {
-		if (printlog(msg)) {
-			Log.e(tag, msg);
-			printLog("e", tag, msg);
+	public static void d(String tag, String msg, Throwable e) {
+		final String eMsg = throwableMsg(msg, e);
+		if (printLog(LEVEL_DEBUG, eMsg)) {
+			Log.d(tag, msg, e);
+			loggerWriter("d", tag, eMsg);
 		}
 	}
 
-	public static void e(String msg) {
-		if (printlog(msg)) {
-			Log.e(Tag, msg);
-			printLog("e", Tag, msg);
+	public static void v(String msg) {
+		v(Tag, msg);
+	}
+
+	public static void v(String msg, Throwable e) {
+		v(Tag, msg, e);
+	}
+
+	public static void v(String tag, String msg) {
+		if (printLog(LEVEL_VERBOSE, msg)) {
+			Log.d(tag, msg);
+			loggerWriter("v", tag, msg);
 		}
 	}
 
-	public static void printLog(String type, String tag, String msg){
+	public static void v(String tag, String msg, Throwable e) {
+		final String eMsg = throwableMsg(msg, e);
+		if (printLog(LEVEL_VERBOSE, eMsg)) {
+			Log.v(tag, msg, e);
+			loggerWriter("v", tag, eMsg);
+		}
+	}
+
+	private static void loggerWriter(String type, String tag, String msg){
 		if (!PRINT_LOG_2FILE)
 			return;
 		try {
 			LoggerWriter.getInstance().print(type, tag, msg);
 		} catch (Exception e) {
+			Log.d(tag, "loggerWriter", e);
 		}
 	}
 
-	private static boolean printlog(String msg) {
-		return (PRINT_LOG) && !TextUtils.isEmpty(msg);
+	private static boolean printLog(int type, String msg) {
+		return PRINT_LOG && (level <= type) && !TextUtils.isEmpty(msg);
+	}
+
+	private static String throwableMsg(String msg, Throwable e) {
+		String text = "";
+		if (!TextUtils.isEmpty(msg)) {
+			text += msg;
+			text += "		";
+		}
+		if (e != null) {
+			text += e.getMessage();
+		}
+		return text;
 	}
 }
